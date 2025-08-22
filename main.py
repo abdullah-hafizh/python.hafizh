500591c354c28993e4f58a6bf01a1e7051f35b4f
abdullah-hafizh <abdullahhafizhzahid3@gmail.com>
2025-08-23 07:24:02 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from mathematic.circle.area import area_of_circle
+from mathematic.square.area import area_of_square
+from konversi_suhu.celcius import *
+from konversi_suhu.farenheit import farenheit_to_celcius
+from konversi_suhu.kelvin import kelvin_to_celcius
+
+area_of_circle(7)
+area_of_square(2)
+
+print(f'25 C = {celcius_to_farenheit(25)} F')
+print(f'25 C = {celcius_to_kelvin(25)} K')
+
+print(f'50 F = {farenheit_to_celcius(50)} F')
+print(f'50 K = {kelvin_to_celcius(50)} K')
